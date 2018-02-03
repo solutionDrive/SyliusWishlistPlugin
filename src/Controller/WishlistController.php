dc7a9169dee9f7b7328b41ca4b7505f33d4b033d
Tobias Wojtylak <tw@solutiondrive.de>
2018-02-03 17:44:14 +0100

    pass product id to WishlistController

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -3,20 +3,25 @@ declare(strict_types=1);
 
 namespace SolutionDrive\SyliusWishlistPlugin\Controller;
 
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class WishlistController extends Controller
 {
     /**
+     * @param int     $id
      * @param Request $request
-     *
      * @return RedirectResponse
      */
-    public function addToWishListAction(Request $request)
+    public function addToWishListAction(int $id, Request $request)
     {
         $referer = $request->headers->get('referer');
+
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = $this->get('sylius.repository.product');
+        $product = $productRepository->find($id);
 
         $this->addFlash('success', 'Item has been added to wishlist');
         return $this->redirect($referer);
