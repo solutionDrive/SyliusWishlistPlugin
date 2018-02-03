3d428b995e5c3312589145664690463583c6c822
Daniel Hormess <hormess@solutiondrive.de>
2018-02-03 15:17:52 +0100

    - added strict types to wishlist specs - refactored date function names - refactored the function head

--- spec/Entity/WishlistSpec.php
+++ spec/Entity/WishlistSpec.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spec\SolutionDrive\SyliusWishlistPlugin\Entity;
 
-use Prophecy\Argument;
 use PhpSpec\ObjectBehavior;
 
-use Sylius\Component\Core\Model\Customer;
+use Sylius\Component\Core\Model\CustomerInterface;
 
 use SolutionDrive\SyliusWishlistPlugin\Entity\Wishlist;
 use SolutionDrive\SyliusWishlistPlugin\Entity\WishlistInterface;
@@ -17,32 +18,26 @@ class WishlistSpec extends ObjectBehavior
         $this->shouldHaveType(Wishlist::class);
     }
 
-    function it_should_implement_wishlist_interface()
+    function it_implements_wishlist_interface()
     {
         $this->shouldImplement(WishlistInterface::class);
     }
 
-    function it_has_customer(
-        Customer $customer
-    )
+    function it_has_customer(CustomerInterface $customer)
     {
         $this->setCustomer($customer);
         $this->getCustomer()->shouldReturn($customer);
     }
 
-    function it_has_creation_date(
-        \DateTime $dateTime
-    )
+    function it_has_created_at_date(\DateTime $dateTime)
     {
-        $this->setCreationDate($dateTime);
-        $this->getCreationDate()->shouldReturn($dateTime);
+        $this->setCreatedAt($dateTime);
+        $this->getCreatedAt()->shouldReturn($dateTime);
     }
 
-    function it_has_last_updated_date(
-        \DateTime $dateTime
-    )
+    function it_has_updated_at_date(\DateTime $dateTime)
     {
-        $this->setLastUpdatedDate($dateTime);
-        $this->getLastUpdatedDate()->shouldReturn($dateTime);
+        $this->setUpdatedAt($dateTime);
+        $this->getUpdatedAt()->shouldReturn($dateTime);
     }
 }
