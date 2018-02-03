6fecdc57ae19f7dc77ed6a105d7578d16f2d1931
Tobias Wojtylak <tw@solutiondrive.de>
2018-02-03 16:27:27 +0100

    added controller for button

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -1,20 +1,24 @@
 <?php
-
 declare(strict_types=1);
 
 namespace SolutionDrive\SyliusWishlistPlugin\Controller;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class WishlistController extends Controller
 {
-
-    public function addToWishList(Request $request)
+    /**
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function addToWishListAction(Request $request)
     {
         $referer = $request->headers->get('referer');
 
         $this->addFlash('success', 'Added product to wishlist');
-        $this->redirect($referer);
+        return $this->redirect($referer);
     }
 }
